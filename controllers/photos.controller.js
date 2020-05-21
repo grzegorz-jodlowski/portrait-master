1fffa24d0afb3e244f6c4f132c0abe55d0a4e16d
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-05-21 17:24:54 +0200

    Add title, author and email strings validation

--- controllers/photos.controller.js
+++ controllers/photos.controller.js
@@ -11,16 +11,25 @@ exports.add = async (req, res) => {
 
     if (title && title.length <= 25 && author && author.length <= 50 && email && file) { // if fields are not empty...
 
+      const stringPattern = new RegExp(/(<*>(([A-z]|\s)*)<*\/*>)|(([A-z]|\s|\.)*)/, 'g');
+      const emailPattern = new RegExp(/^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/, 'g');
+
+      const titleMatched = title.match(stringPattern).join('');
+      const authorMatched = title.match(stringPattern).join('');
+      const emailMatched = email.match(emailPattern).join('');
+
+      if (titleMatched.length < title.length || authorMatched.length < author.length) throw new Error('Invalid characters...');
+      if (emailMatched.length < email.length) throw new Error('Invalid email address');
+
       const fileName = file.path.split('/').slice(-1)[0]; // cut only filename from full path, e.g. C:/test/abc.jpg -> abc.jpg
       const fileExt = fileName.split('.').slice(-1)[0];
 
-      if (acceptedExtensions.some(ext => ext === fileExt)) {
-        const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
-        await newPhoto.save(); // ...save new photo in DB
-        res.json(newPhoto);
-      } else {
-        throw new Error(`Wrong photo extension! Accept only: ${acceptedExtensions.join(', ')}`);
-      }
+      if (!acceptedExtensions.some(ext => ext === fileExt)) throw new Error('Wrong photo extension!');
+
+      const newPhoto = new Photo({ title, author, email, src: fileName, votes: 0 });
+      await newPhoto.save(); // ...save new photo in DB
+      res.json(newPhoto);
+
     } else {
       throw new Error('Wrong input!');
     }
