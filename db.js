fe9e7735d6cf86dfde43824cd5c45a6201f156fe
Grzegorz Jodłowski <grzegorz.jodlowski@gmail.com>
2020-06-04 18:30:33 +0200

    Change db settings to connect with mongo atlas

--- db.js
+++ db.js
@@ -6,7 +6,8 @@ const loadTestData = require('./testData');
 const connectToDB = () => {
 
   // connect to DB
-  mongoose.connect('mongodb://localhost:27017/photosDB', { useNewUrlParser: true, useUnifiedTopology: true });
+  // mongoose.connect('mongodb://localhost:27017/photosDB', { useNewUrlParser: true, useUnifiedTopology: true });
+  mongoose.connect(`mongodb+srv://gjodlowski:${process.env.photosDB}@cluster0-rm7fq.gcp.mongodb.net/photosDB?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true });
   const db = mongoose.connection;
 
   // on success
